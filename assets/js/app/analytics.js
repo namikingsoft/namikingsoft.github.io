4617fe347e28d6b85c5debab2bfd453160fbdaf4
Tsubasa Namiki <namikingsoft@gmail.com>
2015-09-04 05:01:11 +0900

    Disabled analytics from localhost access

--- assets/js/app/analytics.js
+++ assets/js/app/analytics.js
@@ -3,5 +3,9 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-66829719-1', 'auto');
-ga('send', 'pageview');
+if (location.hostname != 'localhost') {
+  ga('create', 'UA-66829719-1', 'auto');
+  ga('send', 'pageview');
+} else {
+  console.log('disabled google analytics from localhost.');
+}
